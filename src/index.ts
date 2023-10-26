b2ff664cf0bed27d8f24b8af42188cb8698bc77c
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-10-26 12:38:51 +0200

    add  initial run

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,22 @@ import { promisify } from 'node:util'
 import { exec as execCb } from 'node:child_process'
 const exec = promisify(execCb)
 
-console.log('Hello World')
+const library: Record<string, Record<string, number>> = {}
 
-async function read_loc_per_filetypes(path) {
-	const { stdout } = await exec('cloc --vcs=git --json ' + path)
+async function read_loc_per_filetypes(path: string) {
+	const { stdout } = await exec(`cd ${path} && cloc --vcs=git --json .`)
+	const output = JSON.parse(stdout)
+	console.log(stdout, output)
+	for (const [lang, { code }] of Object.entries(output) as [string, { code: number }][]) {
+		if (lang === 'header') continue
+		if (!library[lang]) library[lang] = {}
+		library[lang]['2023-10-26'] = code
+	}
 }
+
+async function main() {
+	await read_loc_per_filetypes('../code')
+	console.log(library)
+}
+
+main()
